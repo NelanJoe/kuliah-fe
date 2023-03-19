752685b6ad33a6f79b9a20c4c325682ba8b8622a
NelanJoe <nelanjorelat@gmail.com>
2023-03-19 15:52:33 +0700

    add: lesson-4 & task-4

---
+++ movie-database/src/components/Footer/index.jsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer>
+      <h4>Copyright &copy; By Nelan</h4>
+      <p>Tahun {new Date().getFullYear()}</p>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ movie-database/src/components/Hello/index.jsx
@@ -0,0 +1,10 @@
+export default function Hello({ name, major }) {
+  return (
+    <div>
+      <h1>Hello, Nama saya {name}</h1>
+      <p>Saya berkuliah dengan jurusan {major}</p>
+      <hr />
+      <br />
+    </div>
+  );
+}
