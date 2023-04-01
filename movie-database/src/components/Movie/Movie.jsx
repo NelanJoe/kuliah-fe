6192def533829c1721300f7765554f5e58041bf7
NelanJoe <nelanjorelat@gmail.com>
2023-04-01 13:13:25 +0700

    init

--- movie-database/src/components/Movie/Movie.jsx
+++ movie-database/src/components/Movie/Movie.jsx
@@ -1,19 +1,12 @@
 import styles from "./Movie.module.css";
 
-const Movie = () => {
-  const date = new Date().getUTCDate();
-  const years = new Date().getFullYear();
-
+const Movie = ({ title, year, poster }) => {
   return (
     <div className={styles.movie}>
-      <img
-        className={styles.movie__image}
-        src="https://source.unsplash.com/random/300x400"
-        alt=""
-      />
+      <img className={styles.movie__image} src={poster} alt={title} />
       <div className={styles.movie__body}>
-        <h4 className={styles.movie__body__title}>Movie Title</h4>
-        <p className={styles.movie__body__date}>{`${date} / ${years}`}</p>
+        <h4 className={styles.movie__body__title}>{title}</h4>
+        <p className={styles.movie__body__date}>{year}</p>
       </div>
     </div>
   );
--- movie-database/src/components/Movies/Movies.jsx
+++ movie-database/src/components/Movies/Movies.jsx
@@ -1,23 +1,38 @@
+import { useState } from "react";
+import data from "../../data/utils/data";
 import Movie from "../Movie/Movie";
 import styles from "./Movies.module.css";
+import { nanoid } from "nanoid";
 
 const Movies = () => {
+  const [movies, setMovies] = useState(data);
+
+  const uniqueID = () => {
+    return Math.floor(Math.random() * 100);
+  };
+
+  const handleClick = () => {
+    const movie = {
+      id: nanoid(10),
+      title: "Spiderman Far From Home",
+      poster: "https://source.unsplash.com/random/300x400",
+      year: new Date().getFullYear(),
+      type: "movie",
+    };
+
+    return setMovies([...movies, movie]);
+  };
+
   return (
     <div className={styles.container}>
       <section className={styles.movies}>
         <h2 className={styles.movies__title}>Latest Movies</h2>
         <div className={styles.movie__container}>
-          <Movie />
-          <Movie />
-          <Movie />
-          <Movie />
-          <Movie />
-          <Movie />
-          <Movie />
-          <Movie />
-          <Movie />
-          <Movie />
+          {movies.map((movie) => (
+            <Movie key={movie.id} {...movie} />
+          ))}
         </div>
+        <button onClick={handleClick}>Add Movie</button>
       </section>
     </div>
   );
